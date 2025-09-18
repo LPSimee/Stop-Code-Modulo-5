2b3d8fb5eb0716463ab6882d05f6931013acba4b
LP Simeon <lp.simeon@yahoo.com>
2025-09-18 20:24:16 +0200

    feat(app component): Added List of booked rooms

--- 1-gestione-stanze/src/app/app.component.ts
+++ 1-gestione-stanze/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export type tipoStanza = "singola" | "doppia" | "tripla"; /* types of rooms */
+
+/* room type */
+export type room = { id: number, tipologia: tipoStanza, descrizione: string, prezzo: number, disponibile: boolean };
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -7,4 +12,57 @@ import { Component } from '@angular/core';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
+    roomList: Array<room>[];
+
+    bookedRooms: Array<room> = [
+        {
+            id: 1,
+            tipologia: "singola",
+            descrizione: "Stanza singola non finestrata",
+            prezzo: 70,
+            disponibile: true
+        },
+        {
+            id: 2,
+            tipologia: "singola",
+            descrizione: "Stanza singola vista piscina",
+            prezzo: 50,
+            disponibile: true
+        },
+        {
+            id: 3,
+            tipologia: "doppia",
+            descrizione: "Stanza doppia matrimoniale",
+            prezzo: 80,
+            disponibile: true
+        },
+        {
+            id: 4,
+            tipologia: "tripla",
+            descrizione: "Stanza tripla con matrimoniale e letto singolo",
+            prezzo: 140,
+            disponibile: true
+        },
+        {
+            id: 5,
+            tipologia: "tripla",
+            descrizione: "Stanza tripla con tre letti singoli",
+            prezzo: 120,
+            disponibile: true
+        },
+        {
+            id: 6,
+            tipologia: "doppia",
+            descrizione: "Stanza doppia con due letti singoli",
+            prezzo: 80,
+            disponibile: true
+        },
+        {
+            id: 7,
+            tipologia: "singola",
+            descrizione: "Stanza singola finestrata",
+            prezzo: 75,
+            disponibile: false
+        }
+    ];
 }
