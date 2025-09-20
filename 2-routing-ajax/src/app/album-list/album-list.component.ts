24fae881124a10a913cb1d8ec83416368325fc4e
LP Simeon <lp.simeon@yahoo.com>
2025-09-20 18:31:51 +0200

    feat(album list): added HTML tags and TS logic

--- 2-routing-ajax/src/app/album-list/album-list.component.ts
+++ 2-routing-ajax/src/app/album-list/album-list.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+import { AlbumService } from '../services/album.service';
 @Component({
-  selector: 'app-album-list',
-  standalone: false,
-  templateUrl: './album-list.component.html',
-  styleUrl: './album-list.component.scss'
+    selector: 'app-album-list',
+    standalone: false,
+    templateUrl: './album-list.component.html',
+    styleUrl: './album-list.component.scss'
 })
 export class AlbumListComponent {
+    albumList: any[]; // Array to hold the list of albums
 
+    constructor(private httpService: AlbumService) { }
+
+    ngOnInit(): void {
+        this.getAlbums();
+    }
+
+    getAlbums(): void {
+        this.httpService.getAlbums().subscribe(
+            {
+                next: (data) => {
+                    console.log(data)
+                    this.albumList = data;
+                },
+                error: (error) => console.error("Error:", error)
+            }
+        );
+    }
 }
