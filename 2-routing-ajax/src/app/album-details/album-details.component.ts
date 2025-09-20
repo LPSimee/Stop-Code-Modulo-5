8ed51a2559400e1ec53ac9a3152e78946097dc3c
LP Simeon <lp.simeon@yahoo.com>
2025-09-20 18:32:45 +0200

    feat(album details): created album-details component

---
+++ 2-routing-ajax/src/app/album-details/album-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-details',
+  standalone: false,
+  templateUrl: './album-details.component.html',
+  styleUrl: './album-details.component.scss'
+})
+export class AlbumDetailsComponent {
+
+}
