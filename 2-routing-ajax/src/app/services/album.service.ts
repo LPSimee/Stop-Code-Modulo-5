a0d99d2269ae7e9dbedc1ee955b989e9131a5c62
LP Simeon <lp.simeon@yahoo.com>
2025-09-20 18:26:31 +0200

    feat(album service): added HTTP GET methods

--- 2-routing-ajax/src/app/services/album.service.ts
+++ 2-routing-ajax/src/app/services/album.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AlbumService {
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    private apiURL = "https://jsonplaceholder.typicode.com";
+
+    /* HTTP GET Methods*/
+
+    // Method to get all albums
+    getAlbums(): Observable<any> {
+        return this.http.get<any>(`${this.apiURL}/albums`);
+    } // getAlbums()
+
+    // Method to get a specific album by its ID
+    getAlbumById(id: number): Observable<any> {
+        return this.http.get<any>(`${this.apiURL}/photos?albumId=${id}`);
+    } // getAlbumsById()
 }
