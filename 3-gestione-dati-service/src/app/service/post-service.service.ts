606c49fea34062b8b2f8912cf329dc5654a2959b
LP Simeon <lp.simeon@yahoo.com>
2025-09-22 21:43:30 +0200

    feat(post-service); added API calls

--- 3-gestione-dati-service/src/app/service/post-service.service.ts
+++ 3-gestione-dati-service/src/app/service/post-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class PostServiceService {
+    constructor(private http: HttpClient) { }
 
-  constructor() { }
+    private apiUrl = 'http://localhost:3000/posts';
+
+    // Method to get all posts
+    getPosts(): Observable<any> {
+        return this.http.get<any>(this.apiUrl);
+    }
+
+    // Method to get posts by category
+    getPostsByCategory(category: string): Observable<any> {
+        return this.http.get<any>(`${this.apiUrl}?category=${category}`);
+    }
 }
