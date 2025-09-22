b66a1409b647811f2d0f1c33d8ac46f3bf99756c
LP Simeon <lp.simeon@yahoo.com>
2025-09-22 21:47:11 +0200

    fix(post-service): renamed "PostServiceService" to "PostService"

--- 3-gestione-dati-service/src/app/app.component.ts
+++ 3-gestione-dati-service/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { PostService } from './service/post.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '3-gestione-dati-service';
+    title = '3-gestione-dati-service';
+
+
+    getPosts() {
+        return "";
+    }
+
+    getPostsByCategory(category: string) {
+        return "";
+    }
 }
--- 3-gestione-dati-service/src/app/service/post.service.ts
+++ 3-gestione-dati-service/src/app/service/post.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 @Injectable({
     providedIn: 'root',
 })
-export class PostServiceService {
+export class PostService {
     constructor(private http: HttpClient) { }
 
     private apiUrl = 'http://localhost:3000/posts';
