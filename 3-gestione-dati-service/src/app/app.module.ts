ddf9ef856ae1332949a57724adf744540ab39454
LP Simeon <lp.simeon@yahoo.com>
2025-09-22 20:08:25 +0200

    feat(header): created header component

--- 3-gestione-dati-service/src/app/app.module.ts
+++ 3-gestione-dati-service/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        FooterComponent
+        FooterComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
