8362e80640de77b4a73ed059dbbf2a2c1eacb46a
LP Simeon <lp.simeon@yahoo.com>
2025-09-22 22:05:04 +0200

    feat(app-comp): added html tags to display the list of posts

--- 3-gestione-dati-service/src/app/app.component.ts
+++ 3-gestione-dati-service/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PostService } from './service/post.service';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -7,14 +8,28 @@ import { PostService } from './service/post.service';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    title = '3-gestione-dati-service';
+    postList: any[]; // Array to hold the list of posts
 
+    constructor(private postService: PostService) { }
 
-    getPosts() {
-        return "";
+    ngOnInit(): void {
+        this.postService.getPosts().subscribe(
+            {
+                next: (data) => {
+                    console.log(data);
+                    this.postList = data;
+                },
+                error: (error) => console.error("Error:", error),
+                complete: () => console.log("Completed")
+            }
+        );
     }
 
+    // getPosts() {
+    //     return this.postService.getPosts();
+    // }
+
     getPostsByCategory(category: string) {
-        return "";
+        return this.postService.getPostsByCategory(category);
     }
 }
